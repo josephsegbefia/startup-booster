690b995e32758d5b15be7a96196de6b07c10edf7
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-10-29 19:18:59 +0000

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { auth, signIn, signOut } from "@/auth";
+// import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = async () => {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          {/* <Image src="/startup_logo.jpg" alt="logo" width={25} height={10} /> */}
+          <h1 className="text-2xl text-black font-work-sans">
+            Startup Booster
+          </h1>
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
